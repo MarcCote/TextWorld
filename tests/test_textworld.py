e72ed2af03ed46227b34f38ca428dcdb8f26ebe1
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2023-11-17 13:57:18 -0500

    Rename Environment.infos to Environment.request_infos

--- tests/test_textworld.py
+++ tests/test_textworld.py
@@ -136,8 +136,6 @@ def test_playing_generated_games():
             # Play the game using RandomAgent and make sure we can always finish the
             # game by following the winning policy.
             env = textworld.start(game_file)
-            env.infos.policy_commands = True
-            env.infos.game = True
 
             agent = textworld.agents.RandomCommandAgent()
             agent.reset(env)
